28093448d7172f063c8cc32631708c0fa0366b4b
GergelyPapa <84639534+GergelyPapa@users.noreply.github.com>
2022-12-16 09:34:36 +0100

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'base', views.base, name='base'),
-    url(r'post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url('post/new/', views.post_new, name='post_new'),
-    url('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from urllib import request
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -9,17 +8,11 @@ from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
-
-def base(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/base.html', {'posts': posts})
-
+    
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
-    
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,9 +25,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
-
-
+    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -47,4 +38,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})   
+    return render(request, 'blog/post_edit.html', {'form': form})
